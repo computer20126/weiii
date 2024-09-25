8be953304a3b9c6d868825fecf357df995a8f3ed
computer20126 <computer20126@gmail.com>
2024-09-26 00:02:50 +0800

    second commit

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法：
   // base: '/Repository 的名稱/'
-  base: '/vitewebsite2/',
+  base: '/https://github.com/computer20126/vitewebsite2/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
